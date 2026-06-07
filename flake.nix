c445cc4643495da5ab464b01f070fa9f50b02c2e
joao <joao@twin.so>
2026-06-07 09:01:47 +0000

    feat: add package

--- flake.nix
+++ flake.nix
@@ -23,8 +23,18 @@
           file = ./rust-toolchain.toml;
           sha256 = "sha256-sqSWJDUxc+zaz1nBWMAJKTAGBuGWP25GCftIOlCEAtA=";
         };
+        zellij = pkgs.zellij.unwrapped.overrideAttrs {
+          version = "0.45.0-fix-slow-attach";
+          src = ./.;
+          cargoDeps = pkgs.rustPlatform.fetchCargoVendor {
+            src = ./.;
+            hash = "sha256-KAmj4KSL80FM6OQiKPFJ82njgnQYrph0IMesCBw4SM4=";
+          };
+        };
       in
       {
+        packages.default = zellij;
+
         devShells.default = pkgs.mkShell {
           packages = [
             rustToolchain
